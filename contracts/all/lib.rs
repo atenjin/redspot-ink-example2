5227805e2a41843a7e981c54cf81da775a9b2844
Aten <jincxmain@gmail.com>
2021-06-23 17:50:53 +0800

    fix

--- contracts/all/lib.rs
+++ contracts/all/lib.rs
@@ -15,22 +15,29 @@ pub mod all {
 
     #[ink(storage)]
     pub struct All {
-        // erc20: Lazy<Erc20>,
-        // erc20_minable: Lazy<Erc20Minable>,
+        erc20: Lazy<Erc20>,
+        erc20_minable: Lazy<Erc20Minable>,
     }
 
     impl All {
         #[ink(constructor)]
         pub fn new(account1: Erc20, account2: Erc20Minable) -> Self {
             Self {
-                // erc20: Lazy::new(account1),
-                // erc20_minable: Lazy::new(account2),
+                erc20: Lazy::new(account1),
+                erc20_minable: Lazy::new(account2),
             }
         }
 
         #[ink(message)]
-        pub fn transfer1(&mut self, to: AccountId) {
-            // (*self.erc20).transfer();
+        pub fn transfer1(&mut self, to: AccountId, value: Balance) {
+            (*self.erc20).transfer(to, value).expect("contract self do not have token")
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, who: AccountId) -> Balance {
+            let value1 = (*self.erc20).balance_of(who);
+            let value2 = (*self.erc20_minable).balance_of(who);
+            value1 + value2
         }
     }
 }
